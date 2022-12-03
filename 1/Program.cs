623a0a73da131529432cb34845d86464962426c9
ihor000 <ihor.silnov@yandex.ru>
2022-12-04 01:49:35 +0300

    text

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,5 +1,16 @@
 ﻿Console.WriteLine("Введите трех значное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int secondNumber = (number /10) % 10; 
 
-Console.WriteLine(secondNumber);
+
+if(number >= 100)
+{
+    number = (number / 10) % 10;
+
+    Console.Write("Вторая цифра трех значного числа: ");
+    Console.WriteLine(number);
+       
+}
+else 
+{
+   Console.WriteLine("число не является трех значным");
+}
