86902eeeca838d24ccece26b684612de9a8aeac7
ihor000 <ihor.silnov@yandex.ru>
2022-12-04 03:00:47 +0300

    text

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите трех значное число: ");
+﻿Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 
@@ -6,7 +6,7 @@ if (number > 100 && number < 999)
 
 {
     number = (number / 10) % 10;
-    Console.Write("Вторая цифра трех значного числа: ");
+    Console.Write("Вторая цифра трехзначного числа: ");
     Console.WriteLine(number); 
 }
 
