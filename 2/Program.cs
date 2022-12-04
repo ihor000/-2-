1036d4879bb1b8dd0d50d6772e4ba50bfc22bd79
ihor000 <ihor.silnov@yandex.ru>
2022-12-05 00:09:47 +0300

    text

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ввидите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1;
+int number2;
+
+
+if (number > 100) 
+{
+    number1 = number;
+            number = number % 10;
+    Console.WriteLine(number);    
+}
+
+
+else
+{
+   Console.WriteLine("Введено не верное число");
+}
+
+
