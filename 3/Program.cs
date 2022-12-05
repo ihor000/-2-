e3deb63d6103e8effbe302378e0d8becf33ca571
ihor000 <ihor.silnov@yandex.ru>
2022-12-05 23:00:48 +0300

    text

---
+++ 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 7 == 0 && number % 23 == 0 )
+{
+   Console.WriteLine("Число кратное:");
+}
+else
+{
+  Console.WriteLine("Не является кратным:"); 
+}
